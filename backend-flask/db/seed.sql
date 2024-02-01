959de521defcceaa436274000f2239e28fa8fb50
Pukhrambam Samir Singh <126311249+PukhrambamSamirSingh@users.noreply.github.com>
2024-02-01 14:56:54 +0000

    fixed in scripting paths

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,7 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
     ('Samir Singh', 'pinkiesinha22@gmail.com', 'samirsingh', 'MOCK'),
-    ('Robin Singh', 'probinsingh234@gmail.com', 'robinsingh', 'MOCK');
+    ('Robin Singh', 'probinsingh234@gmail.com', 'robinsingh', 'MOCK'),
     ('Londo Mollari', 'lmollari@centari.com', 'londo', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
