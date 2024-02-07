4c1465577f668c1c29f8778107ee31545ff2eb59
Pukhrambam Samir Singh <robinsamir810@gmail.com>
2024-02-07 13:33:44 +0000

    implementing the frontend.

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -7,11 +7,11 @@ tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run(cognito_user_id=None):
     print("HOME ACTIVITY")
-    #logger.info("HomeActivities")
-    with tracer.start_as_current_span("home-activites-mock-data"):
-      span = trace.get_current_span()
-      now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat())
+    # logger.info("HomeActivities")
+    # with tracer.start_as_current_span("home-activites-mock-data"):
+    #   span = trace.get_current_span()
+    #   now = datetime.now(timezone.utc).astimezone()
+    #   span.set_attribute("app.now", now.isoformat())
     
     sql = db.template('activities','home')
     results = db.query_array_json(sql)
